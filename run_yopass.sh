dc56e86861185c1bf70780d61bb2eb0494e6b2b9
Aaron Hine <105221222+roo-ahine@users.noreply.github.com>
2024-06-11 12:49:18 +0100

    Seceng 170/deploy yopass hopper staging (#9)
    * yopass staging app
    
    * update hopper config
    
    * fix tf
    
    * update owner group
    
    * set desiredCount in Hopper config
    
    * Update Dockerfile and remove features from UI in staging
    
    * add lb healthcheck
    
    * update lb status code
    
    * enable logging

--- run_yopass.sh
+++ run_yopass.sh
@@ -4,4 +4,5 @@
 memcached -u memcache &
 
 # Start the yopass server 
-/yopass-server --memcached=127.0.0.1:11211 --port 80 > /dev/null 2>&1
+#/yopass-server --memcached=127.0.0.1:11211 --port 80 > /dev/null 2>&1
+/yopass-server --memcached=127.0.0.1:11211 --port 80
