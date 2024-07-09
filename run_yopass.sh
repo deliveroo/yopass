e4fb2eb99025e154db87cd96fbf90b0f60d93261
Aaron Hine <105221222+roo-ahine@users.noreply.github.com>
2024-07-09 15:44:39 +0100

    Seceng 170/deploy yopass hopper staging (#25)
    * yopass staging app
    
    * update hopper config
    
    * fix tf
    
    * update owner group
    
    * set desiredCount in Hopper config
    
    * Update Dockerfile and remove features from UI in staging
    
    * add lb healthcheck
    
    * update lb status code
    
    * enable logging
    
    * Remove comment displaying in the UI
    
    * set one-time password only and set logging level to debug
    
    * add redis instance for data persistence
    
    * fix dockerfile
    
    * update terraform
    
    * update terraform
    
    * fix tf
    
    * fix redis url path
    
    * fix redis url var
    
    * add netcat to troubleshoot networking issue
    
    * add curl to troubleshoot an issue
    
    * fix engine version for redis
    
    * update tf
    
    * update run script
    
    * update redis tf
    
    * update env var

--- run_yopass.sh
+++ run_yopass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start the yopass server 
-/yopass-server --database redis --redis ${REDIS_URL} --port 80 --force-onetime-secrets --log-level=debug
+/yopass-server --database redis --redis ${REDIS_CACHE_URL} --port 80 --force-onetime-secrets --log-level=debug
