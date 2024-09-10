30d4be015895d15bf71df3e6af1442eb1c2c604f
Aaron Hine <aaron.hine@deliveroo.co.uk>
2024-09-10 11:42:32 +0100

    updated docker-compose config

--- selenium_browser/yopass_secret_test.py
+++ selenium_browser/yopass_secret_test.py
@@ -26,8 +26,8 @@ service = Service('/usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Set the URL
-#url = 'http://yopass_app'
-url = 'http://host.docker.internal'
+url = 'http://yopass'
+#url = 'http://host.docker.internal'
 
 # Set test secret
 test_secret = 'this is a Selenium test secret'
