186201772777ac5869c6e2c6d8177348acf86d3e
Aaron Hine <aaron.hine@deliveroo.co.uk>
2024-12-19 11:10:12 +0000

    put back infra

--- infrastructure/modules/yopass/app.tf
+++ infrastructure/modules/yopass/app.tf
@@ -17,65 +17,65 @@ module "yopass" {
   team_name     = local.supported_team_names["SECURITY_ENGINEERING"]
 }
 
-# module "yopass_web" {
-#   source  = "terraform-registry.deliveroo.net/deliveroo/roo-service/aws"
-#   version = "~> 2.0"
+module "yopass_web" {
+  source  = "terraform-registry.deliveroo.net/deliveroo/roo-service/aws"
+  version = "~> 2.0"
 
-#   service_type = "public_web"
+  service_type = "public_web"
 
-#   application    = module.yopass.config
-#   service_name   = "web"
-#   container_port = 80
+  application    = module.yopass.config
+  service_name   = "web"
+  container_port = 80
 
-#   health_check_codes       = "200"
-#   health_check_path        = "/"
-#   alb_anomaly_bounds       = 6
-#   alb_anomaly_should_alert = false
-#   health_check_interval    = 30
-# }
+  health_check_codes       = "200"
+  health_check_path        = "/"
+  alb_anomaly_bounds       = 6
+  alb_anomaly_should_alert = false
+  health_check_interval    = 30
+}
 
-# module "yopass_web_identity" {
-#   source  = "terraform-registry.deliveroo.net/deliveroo/identity_auth/aws"
-#   version = "~> 6.0"
+module "yopass_web_identity" {
+  source  = "terraform-registry.deliveroo.net/deliveroo/identity_auth/aws"
+  version = "~> 6.0"
 
-#   ecs_service_name = module.yopass_web.service_name
-#   env_name         = var.env_name
-#   extra_scopes     = ["employee", "engineer.contractor"]
-#   lb_listener_arn  = module.yopass_web.lb_listener_arn
-#   redirect_uris    = ["https://${var.redirect_uri}/oauth2/idpresponse"]
-#   target_group_arn = module.yopass_web.target_group_arn
-# }
+  ecs_service_name = module.yopass_web.service_name
+  env_name         = var.env_name
+  extra_scopes     = ["employee", "engineer.contractor"]
+  lb_listener_arn  = module.yopass_web.lb_listener_arn
+  redirect_uris    = ["https://${var.redirect_uri}/oauth2/idpresponse"]
+  target_group_arn = module.yopass_web.target_group_arn
+}
 
-# module "yopass_redis" {
-#   source  = "terraform-registry.deliveroo.net/deliveroo/redis/aws"
-#   version = "~> 9.0"
+module "yopass_redis" {
+  source  = "terraform-registry.deliveroo.net/deliveroo/redis/aws"
+  version = "~> 9.0"
 
-#   application_name           = module.yopass.app_name
-#   replication_group_id       = "yopass-cache"
-#   use_as_store               = false
-#   at_rest_encryption         = false
-#   transit_encryption_enabled = false
-#   cluster_mode_enabled       = false
-#   multi_az_enabled           = false
-#   automatic_failover_enabled = true
-#   num_node_groups            = 1
-#   node_count                 = 1
-#   replicas                   = 1
+  application_name           = module.yopass.app_name
+  replication_group_id       = "yopass-cache"
+  use_as_store               = false
+  at_rest_encryption         = false
+  transit_encryption_enabled = false
+  cluster_mode_enabled       = false
+  multi_az_enabled           = false
+  automatic_failover_enabled = true
+  num_node_groups            = 1
+  node_count                 = 1
+  replicas                   = 1
 
-#   datadog_pagerduty_service = ""
-#   engine_version            = "7.1"
-#   parameter_group_family    = "redis7"
-#   instance_type             = "cache.t3.small"
+  datadog_pagerduty_service = ""
+  engine_version            = "7.1"
+  parameter_group_family    = "redis7"
+  instance_type             = "cache.t3.small"
 
-#   parameter_group_configuration = {
-#     "cluster-enabled"  = "no"
-#     "maxmemory-policy" = "allkeys-lru"
-#   }
-# }
+  parameter_group_configuration = {
+    "cluster-enabled"  = "no"
+    "maxmemory-policy" = "allkeys-lru"
+  }
+}
 
-# resource "hopper_variable" "redis_url" {
-#   app_name   = module.yopass.app_name
-#   name       = "REDIS_CACHE_URL"
-#   value      = module.yopass_redis.url
-#   write_only = true
-# }
+resource "hopper_variable" "redis_url" {
+  app_name   = module.yopass.app_name
+  name       = "REDIS_CACHE_URL"
+  value      = module.yopass_redis.url
+  write_only = true
+}
