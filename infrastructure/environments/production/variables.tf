76323bc274225c27d9d9adc25ea1b3750d6be8e8
Aaron Hine <aaron.hine@deliveroo.co.uk>
2025-01-21 17:10:01 +0000

    update prod infra and conflict

--- infrastructure/environments/production/variables.tf
+++ infrastructure/environments/production/variables.tf
@@ -1,9 +1,9 @@
-# variable "env_name" { default = "production" }
-# variable "shard" { default = "global" }
-# variable "region" { default = "eu-west-1" }
-# variable "datadog_api_key" {}
-# variable "datadog_app_key" {}
+variable "env_name" { default = "production" }
+variable "shard" { default = "global" }
+variable "region" { default = "eu-west-1" }
+variable "datadog_api_key" {}
+variable "datadog_app_key" {}
 
-# data "roo_aws_account" "current" {}
+data "roo_aws_account" "current" {}
 
-# data "roo_tags" "defaults" {}
+data "roo_tags" "defaults" {}
